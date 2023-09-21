9918d2c188b28557938c14c2aadbaab4a15e9dbb
hamoumi3u <hicham.hamoumi9@etu.univ-lorraine.fr>
2023-09-21 15:00:47 +0200

    Boucle de jeu

--- jeupocmon/src/Jeu.java
+++ jeupocmon/src/Jeu.java
@@ -1,8 +1,33 @@
-public class Jeu {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Jeu {
     public static void main(String[] args) {
         Joueur joueur = new Joueur();
         Labyrinthe lab = new Labyrinthe();
-        lab.printLab(joueur);
+        System.out.println("Bienvenu sur pocmon/nAppuyer sur entree pour démarer le jeu ");
+        InputStreamReader inputStream = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStream);
+        System.out.println("Voulez-vous demarer la partie");
+        String test = null;
+        try {
+            test = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while (test.equals("o"))
+        {
+            lab.printLab(joueur);
+            inputStream = new InputStreamReader(System.in);
+            bufferedReader = new BufferedReader(inputStream);
+            System.out.println("Voulez-vous continuer la partie");
+            try {
+                test = bufferedReader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
