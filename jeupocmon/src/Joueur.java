05ad43a42c86bae4827f6b1e23bda760f5a52537
nicola169u <61577365+nicola169u@users.noreply.github.com>
2023-09-28 16:55:54 +0200

    modif dans Personnage et création d'un monstre qui se déplace aléatoirement dans le labyrinthe en meme temps que le joueur

--- jeupocmon/src/Joueur.java
+++ jeupocmon/src/Joueur.java
@@ -1,6 +1,8 @@
 public class Joueur extends Personnage {
     public Joueur() {
         super();
+        posX = 5;
+        posY = 5;
     }
 
     public Direction choisirDirection(String lettre)
@@ -20,40 +22,6 @@ public class Joueur extends Personnage {
 
     }
 
-    public void avancer(Direction direction, Labyrinthe lab){
-        switch (direction){
-            case GAUCHE:
-                if(!lab.getCase(this.posX, this.posY - 1).estMur()){
-                    this.posY -= 1;
-                }else{
-                    System.out.println("On fonce droit dans un mur inconscient !!");
-                }
-                break;
-            case DROITE:
-                if(!lab.getCase(this.posX, this.posY + 1).estMur()){
-                    this.posY += 1;
-                }else{
-                    System.out.println("On fonce droit dans un mur inconscient !!");
-                }
-                break;
-            case BAS:
-                if(!lab.getCase(this.posX, this.posX + 1).estMur()){
-                    this.posX += 1;
-                }else{
-                    System.out.println("On fonce droit dans un mur inconscient !!");
-                }
-                break;
-            case HAUT:
-                if(!lab.getCase(this.posX, this.posX - 1).estMur()){
-                    this.posX -= 1;
-                }else{
-                    System.out.println("On fonce droit dans un mur inconscient !!");
-                }
-                break;
-        }
-
-
-    }
 
     public int getPosX(){
         return this.posX;
